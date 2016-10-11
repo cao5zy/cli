7d21ce0b1e1a574c3980e8e8b03e5fb71d83b2b7
alan_cao <zongying.cao@hpe.com>
2016-10-11 17:02:00 +0800

    添加文件

---
+++ src/createIndexTs.py
@@ -0,0 +1,8 @@
+﻿# -*- coding:utf-8 -*-
+# 该命令用于typescript项目，用于创建目录下的index.ts文件
+# 该命令目前只在python 3.4上测试
+# 请将当前目录设添加到环境变量，这样才能够直接运行命令。
+# 使用方法：
+# 1. 打开cmd
+# 2. 切换到typescript项目的目录
+# 3. 运行py_createindex.bat
